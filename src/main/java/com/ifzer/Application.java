4f3c4046897aa6d68231c643b427296b05bd55e3
bangsen.yin <020lilin@gmail.com>
2018-04-18 21:00:23 -0500

    Add files via upload

---
+++ src/main/java/com/ifzer/Application.java
@@ -0,0 +1,15 @@
+package com.ifzer;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by nelson on 2018-04-19.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
